7f273e08c94a5e54b229282e400fab978e6b1c29
Alessandro <alessandro.boscato@gmail.com>
2020-10-06 16:22:31 +0200

    1° tentativo foreach(errore)

--- index.php
+++ index.php
@@ -26,19 +26,6 @@ Ci sono diverse domande con relative risposte. Gestire il “Database” e la vi
       "answer" => "In alcuni casi sì. Quando fai clic su un risultato della Ricerca Google, il tuo browser web potrebbe reindirizzare alla pagina web di destinazione anche l'indirizzo Internet, o URL, della pagina dei risultati di ricerca sotto forma di URL referrer. Talvolta, l'URL della pagina dei risultati di ricerca potrebbe contenere la query di ricerca che hai inserito. Se utilizzi la ricerca SSL (la funzione di ricerca criptata di Google), nella maggior parte dei casi i termini di ricerca non vengono inviati come parte dell'URL negli URL referrer. Questo comportamento può fare eccezione, ad esempio se utilizzi alcuni browser meno diffusi. Ulteriori informazioni sulla ricerca SSL sono disponibili qui. Le query di ricerca o le informazioni contenute nell'URL referrer potrebbero essere disponibili mediante Google Analytics o un'API (Application Programming Interface). Inoltre, gli inserzionisti potrebbero ricevere informazioni relative all' esatte parole chiave che hanno determinato il clic su un annuncio."
     ]
   ];
-
-  foreach ($faqs as $faq) {
-    var_dump($faq);
-  }
-
-  // $roles = [
-  // 'teachers' => ['Fabio', 'Michele'],
-  // 'PM' => ['Roberto']
-  // ];
-  //
-  // foreach ($roles as $role) {
-  // var_dump($role);
-  // }
 ?>
 
 <!DOCTYPE html>
@@ -50,6 +37,20 @@ Ci sono diverse domande con relative risposte. Gestire il “Database” e la vi
   <title>Google FAQs</title>
 </head>
 <body>
+  <ul>
+    <?php foreach ($faqs as $faq) { ?>
+    <li>
+      <?php foreach ($faq as $k => $value) {
+        if ($key == "question") {
+          echo("<h2>".$value."</h2>");
+        } else ($key == "answer") {
+          echo("<p>".$value."</p>");
+        }
+      }
+      ?>
+    </li>
+    <?php } ?>
+  </ul>
 
 </body>
 </html>

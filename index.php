37411560392481da186ded41b598aaf96bf43339
Alessandro <alessandro.boscato@gmail.com>
2020-10-06 14:49:35 +0200

    aggiunta consegna e struttura html

---
+++ index.php
@@ -0,0 +1,19 @@
+<!-- Riscrivere questa pagina del sito google https://policies.google.com/faq.
+Ci sono diverse domande con relative risposte. Gestire il “Database” e la visualizzazione di queste domande e risposte con PHP. -->
+<?php
+$variable = "string"
+echo $variable
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Google FAQs</title>
+</head>
+<body>
+
+</body>
+</html>
